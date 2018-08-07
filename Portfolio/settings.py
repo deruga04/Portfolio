a26b60c6cd2fa610657a9c9b5a2724f7e158ddd7
deruga <derekli.3628@gmail.com>
2018-08-07 02:18:58 -0400

    Squish

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+os.environ['HTTPS'] = "on"
+os.environ['wsgi.url_scheme'] = 'https'
