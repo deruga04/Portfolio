657ec6ac517bbc47aa224d0eb68f1744c7fafe36
deruga <derekli.3628@gmail.com>
2018-08-07 01:15:22 -0400

    Squish

--- apps/urls.py
+++ apps/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('igbb/',views.igbb, name='igbb')
+    path('igbb/',views.igbb, name='igbb'),
+    path('.well-known/acme-challenge/zrr-5ptgD5aKNsy-erik3hFivxUFLlmCSvFHI-nrZxY', views.ssl, name='acme'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -6,3 +6,6 @@ def index(req):
 
 def igbb(req):
     return render(req, 'igbb.html')
+
+def ssl(req):
+    return 'zrr-5ptgD5aKNsy-erik3hFivxUFLlmCSvFHI-nrZxY.ZHRLRKR1M7CWxOC4CIh4zK3jYb_Ec94b37iS9CD2DQI'
