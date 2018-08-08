f04424111f740ec909e1bac4f7f32d6b8521c2e2
deruga <derekli.3628@gmail.com>
2018-08-08 11:57:33 -0400

    Front page

--- apps/views.py
+++ apps/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def index(req):
-    return render(req, 'index.html')
+    greetings = ["Hello MTV, welcome to my crib!", "안녕하세요", "Nice to meet 'cha", "Welcome"]
+
+    return render(req, 'index.html', context={"greeting": random.choice(greetings)})
+
 
 def igbb(req):
     return render(req, 'igbb.html')
