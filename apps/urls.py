9c58cdd2b489f41fdb709f836cb59038a589ef8f
deruga <derekli.3628@gmail.com>
2018-08-08 11:59:47 -0400

    Removed certbot stuff

--- apps/urls.py
+++ apps/urls.py
@@ -5,5 +5,4 @@ from django.conf.urls import include
 urlpatterns = [
     path('', views.index, name='index'),
     path('igbb/',views.igbb, name='igbb'),
-    path('.well-known/acme-challenge/zrr-5ptgD5aKNsy-erik3hFivxUFLlmCSvFHI-nrZxY', views.ssl, name='acme'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -3,13 +3,10 @@ import random
 
 # Create your views here.
 def index(req):
-    greetings = ["Hello MTV, welcome to my crib!", "안녕하세요", "Nice to meet 'cha", "Welcome"]
+    greetings = ["Hello MTV, welcome to my crib!", "안녕하세요!", "Nice to meet 'cha", "Welcome"]
 
     return render(req, 'index.html', context={"greeting": random.choice(greetings)})
 
 
 def igbb(req):
     return render(req, 'igbb.html')
-
-def ssl(req):
-    return render(req, 'acme')
