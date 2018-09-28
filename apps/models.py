3176b790b0b95559bb34307c5500e571b23d7d58
deruga <derekli.3628@gmail.com>
2018-09-28 11:12:21 -0400

    new template lol

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a title for the project')
+    description = models.TextField()
+    image = models.ImageField()
+    link = models.URLField()
+
+    def __str__():
+        return f'{self.name}\n\n{self.description}'
